9daf19ed21115bd4df26098671e858778f9fe399
Milosz Jakubanis <milosz.jakubanis@codacy.com>
2024-12-16 13:49:07 +0000

    wip, more unused vals

--- src/main/scala/com/miloszjakubanis/filebrowser/Main.scala
+++ src/main/scala/com/miloszjakubanis/filebrowser/Main.scala
@@ -9,6 +9,10 @@ object Main extends IOApp.Simple {
   val run: IO[Unit] = {
     val unusedVal = true
     val unsuedVal2 = "unused"
+    val unsuedVal3 = "unused"
+    val unsuedVal4 = "unused"
+    val unsuedVal5 = "unused"
+    val unsuedVal6 = "unused"
     server.runServer[IO]
   }
 
