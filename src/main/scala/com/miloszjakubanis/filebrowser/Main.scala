3949e0eb1a33a47320db317b49bdb537742cdb8f
Milosz Jakubanis <milosz.jakubanis@codacy.com>
2024-12-16 14:13:28 +0000

    Added more unused lines of code

--- src/main/scala/com/miloszjakubanis/filebrowser/Main.scala
+++ src/main/scala/com/miloszjakubanis/filebrowser/Main.scala
@@ -13,6 +13,9 @@ object Main extends IOApp.Simple {
     val unsuedVal4 = "unused"
     val unsuedVal5 = "unused"
     val unsuedVal6 = "unused"
+    val unsuedVal7 = "unused" //Another 3 lines added
+    val unsuedVal8 = "unused"
+    val unsuedVal9 = "unused"
     server.runServer[IO]
   }
 
